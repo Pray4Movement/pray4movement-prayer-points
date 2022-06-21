eac484f3683c774dd7116d43783347622b1e217c
Prykon <dariomanoukian@gmail.com>
2022-06-21 10:05:24 -0300

    Can show prayer points in public ui

--- functions/shortcodes.php
+++ functions/shortcodes.php
@@ -1,8 +1,54 @@
 <?php
 add_shortcode( 'p4m_prayer_libraries', 'show_prayer_libraries' );
 
-function show_prayer_libraries() {
+function show_prayer_points( $library_id ) {
+    ?>
+<script>
+    function loadPrayerPoints(libraryId) {
+        jQuery.ajax({
+            type: 'POST',
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            url: window.location.origin + `/wp-json/pray4movement-prayer-points/v1/get_prayer_points/${libraryId}`,
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader('X-WP-Nonce', '<?php echo esc_attr( wp_create_nonce( 'wp_rest' ) ); ?>' );
+            },
+            success: function(response) {
+                jQuery('#p4m-library-spinner').remove();
+                jQuery('.p4m-libraries-table').append(`
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Title</th>
+                        <th>Reference</th>
+                        <th>Content</th>
+                        <th>Tags</th>
+                    <tr>
+                </thead>
+                `);
+                response.forEach( function(prayer){
+                    jQuery('#p4m-spinner-row').remove();
+                    jQuery('.p4m-libraries-table').append(`
+                    <tbody>
+                        <tr>
+                            <td>${prayer['id']}</td>
+                            <td>${prayer['title']}</td>
+                            <td>${prayer['reference']}</td>
+                            <td>${prayer['content']}</td>
+                            <td>${prayer['tags']}</td>
+                        </tr>
+                    </tbody>
+                    `);
+                });
+            },
+        });
+    }
+    loadPrayerPoints('<?php echo esc_html( $library_id ); ?>');
+</script>
+    <?php
+}
 
+function show_prayer_libraries() {
     ?>
     <style>
         .p4m-libraries-table {
@@ -11,29 +57,44 @@ function show_prayer_libraries() {
             text-align: center;
         }
     </style>
+    <?php
+    if ( isset( $_GET['library_id'] ) ) {
+        show_prayer_points( sanitize_text_field( wp_unslash( $_GET['library_id'] ) ) );
+        return;
+    }
+    ?>
     <script>
         function loadLibraries() {
             jQuery.ajax({
-                            type: 'POST',
-                            contentType: 'application/json; charset=utf-8',
-                            dataType: 'json',
-                            url: window.location.origin + '/wp-json/pray4movement-prayer-points/v1/get_prayer_libraries',
-                            beforeSend: function(xhr) {
-                                xhr.setRequestHeader('X-WP-Nonce', '<?php echo esc_attr( wp_create_nonce( 'wp_rest' ) ); ?>' );
-                            },
-                            success: function(response) {
-                                response.forEach( function(library){
-                                    console.log(library);
-                                    jQuery('#p4m-spinner-row').remove();
-                                    jQuery('.p4m-libraries-table').append(`
-                                    <tr>
-                                        <td><a href="#">${library['name']}</a></td>
-                                        <td><a href="javascript:downloadCSV(${library['id']}, '${library['key']}')">csv</a></td>
-                                    </tr>
-                                    `);
-                                });
-                            },
-                        });
+                type: 'POST',
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
+                url: window.location.origin + '/wp-json/pray4movement-prayer-points/v1/get_prayer_libraries',
+                beforeSend: function(xhr) {
+                    xhr.setRequestHeader('X-WP-Nonce', '<?php echo esc_attr( wp_create_nonce( 'wp_rest' ) ); ?>' );
+                },
+                success: function(response) {
+                    jQuery('.p4m-libraries-table').append(`
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    `);
+                    response.forEach( function(library){
+                        jQuery('#p4m-library-spinner').remove();
+                        jQuery('.p4m-libraries-table').append(`
+                        <tbody>
+                            <tr>
+                                <td><a href="?library_id=${library['id']}">${library['name']}</a></td>
+                                <td><a href="javascript:downloadCSV(${library['id']}, '${library['key']}')">csv</a></td>
+                            </tr>
+                        </tbody>
+                        `);
+                    });
+                },
+            });
         }
 
         function downloadCSV( libraryId, fileName='pray4movement_prayer_library_download' ) {
